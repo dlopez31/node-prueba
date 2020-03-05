256abf07b8965a99b99ddac85a59715d786cb153
Denys Lopez <denys.lopez.pinate@everis.com>
2020-03-05 13:09:52 -0300

    15 listo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 const express = require('express');
-cosnt { countries } = require('countries-list')
-const { info, error } = require('./modules/my-log');
+const routes  = require('./routes');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('HELLO');
-});
-
-app.get('/info', (request, response) => {
-  response.send('info');
-});
-
-app.get('/country', (req, res) => {
-
-});
-
-app.get('*', (request, response) => {
-  response.status(404).send('NO FOUND');
-});
+routes(app);
 
 app.listen(4000, () => {
   console.log('runig on 4000');
